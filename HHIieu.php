897201e575cd8b21648c8d8ceab50999994e8bb0
thson0404 <107702116+thson0404@users.noreply.github.com>
2022-06-26 23:02:13 +0700

    Update HHIieu.php

--- HHIieu.php
+++ HHIieu.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.1.132';  // CHANGE THIS
+$ip = '192.168.147.131';  // CHANGE THIS
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
